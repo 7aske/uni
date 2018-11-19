d6da73ced0cdb5e97463c53fff48806e6378a75f
Nikola Tasic <ntasic7@gmail.com>
2018-11-19 14:11:34 +0100

    it101-pz-v1.1

--- it101/it101-pz-nikola_tasic_3698/assets/js/team.js
+++ it101/it101-pz-nikola_tasic_3698/assets/js/team.js
@@ -1,5 +1,11 @@
 const cards = document.querySelectorAll('.card');
 cards.forEach(c => {
-	c.addEventListener('mouseenter', e => {});
-	c.addEventListener('mouseleave', e => {});
+	c.addEventListener('mouseenter', e => {
+		c.style.color = 'black';
+		c.style.backgroundColor = 'white';
+	});
+	c.addEventListener('mouseleave', e => {
+		c.style.color = 'white';
+		c.style.backgroundColor = '#343434';
+	});
 });

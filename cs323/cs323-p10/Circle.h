6254885fbcd93ac5c498b576c35ff6e49e67bac7
Nikola Tasic <ntasic7@gmail.com>
2019-05-07 23:57:46 +0200

    cs323p10/v10

---
+++ cs323/cs323-p10/Circle.h
@@ -0,0 +1,16 @@
+//
+// Created by nik on 5/7/19.
+//
+
+#ifndef CS323_P10_CIRCLE_H
+#define CS323_P10_CIRCLE_H
+
+
+
+class Circle {
+
+};
+
+
+
+#endif //CS323_P10_CIRCLE_H
---
+++ cs323/cs323-p10/Cylinder.h
@@ -0,0 +1,16 @@
+//
+// Created by nik on 5/7/19.
+//
+
+#ifndef CS323_P10_CYLINDER_H
+#define CS323_P10_CYLINDER_H
+
+
+
+class Cylinder {
+
+};
+
+
+
+#endif //CS323_P10_CYLINDER_H

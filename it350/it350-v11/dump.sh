d2b8b507107f6c98e970378e31e2b0ed5e37f7ab
Nikola Tasic <ntasic7@gmail.com>
2019-12-11 16:19:08 +0100

    it350-v11

---
+++ it350/it350-v11/dump.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+mysqldump -u nik -p --add-drop-database --all-databases > ~/Code/uni/it350/it350-v11/v11.sql
+# mysql -u nik -p
+# create database studentska;
+# use studentska;
+# mysqldump -u nik -p < ~/Code/uni/it350/it350-v11/v11.sql

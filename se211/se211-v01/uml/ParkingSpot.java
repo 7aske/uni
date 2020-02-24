ebf7778ceedfd0be2efa21dd6a566e7e6dfae136
Nikola Tasic <ntasic7@gmail.com>
2020-02-24 14:09:57 +0100

    se211-v01

---
+++ se211/se211-v01/uml/ParkingSpot.java
@@ -0,0 +1,11 @@
+/***********************************************************************
+ * Module:  ParkingSpot.java
+ * Author:  nik
+ * Purpose: Defines the Class ParkingSpot
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 8793b1b8-f998-44f9-b462-b8b25546f844 */
+public class ParkingSpot {
+}

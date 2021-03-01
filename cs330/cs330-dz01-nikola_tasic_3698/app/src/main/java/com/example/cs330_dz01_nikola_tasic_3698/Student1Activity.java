ba42d3d4207b62cb7a431144468787464fae04e8
Nikola Tasic <ntasic7@gmail.com>
2021-03-02 00:19:08 +0100

    cs330-dz01

---
+++ cs330/cs330-dz01-nikola_tasic_3698/app/src/main/java/com/example/cs330_dz01_nikola_tasic_3698/Student1Activity.java
@@ -0,0 +1,14 @@
+package com.example.cs330_dz01_nikola_tasic_3698;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

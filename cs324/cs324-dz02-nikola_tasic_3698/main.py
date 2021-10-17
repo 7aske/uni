41aea68ad3c5b57b8bbfdfa49daeb6bc1d94bd23
Nikola Tasic <ntasic7@gmail.com>
2021-10-17 23:02:45 +0200

    cs324 - dz02

---
+++ cs324/cs324-dz02-nikola_tasic_3698/main.py
@@ -0,0 +1,28 @@
+import math
+
+
+def is_prime(num: int):
+	if not isinstance(num, int):
+		raise ValueError
+
+	if num <= 1:
+		return False
+
+	if 2 <= num <= 3:
+		return True
+
+	for i in range(2, math.ceil(math.sqrt(num)) + 1):
+		if num % i == 0:
+			return False
+
+	return True
+
+
+def main():
+	for n in range(10000):
+		if is_prime(n):
+			print(n)
+
+
+if __name__ == '__main__':
+	main()

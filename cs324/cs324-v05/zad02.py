5137d4b15c3e6293cef3849e3123db1ae6dc7796
Nikola Tasic <ntasic7@gmail.com>
2021-11-02 13:42:16 +0100

    cs324 - v05

---
+++ cs324/cs324-v05/zad02.py
@@ -0,0 +1,17 @@
+class GeometrijskaFigura:
+	def __init__(self, *edges):
+		self.edges = edges
+
+	def area(self):
+		pass
+
+	def circumference(self):
+		return sum(self.edges)
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
